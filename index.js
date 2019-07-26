d083b1b33689b9aa75cff9b7dfe898e021d41159
Michael Armes <michael@un-loop.org>
2019-07-26 13:49:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const Router = require("koa-router");
+const compose = require("koa-compose");
+
+const supportedMethods = [
+    "get",
+    "post",
+    "put",
+    "link",
+    "unlink",
+    "delete",
+    "del",
+    "head",
+    "options",
+    "patch",
+    "all"
+];
+
+module.exports = (router) => {
+    router = router || new Router();
+
+    const route = (method, status, ...middleware) => {
+
+        const composed = compose(...middleware);
+        const routeMiddleware  = async (ctx, next) => {
+            await next();
+
+            const finished = () => {};
+
+            if (ctx.status === status) {
+                await composed(ctx, finished);
+            } else {
+                finished();
+            }
+        }
+        
+        return router[method]("/", routeMiddleware);
+    }
+
+    for(let method in supportedMethods) {
+        this[method] = route.bind(this, method);
+    }
+
+    this.middleware = router.middleware;
+}
